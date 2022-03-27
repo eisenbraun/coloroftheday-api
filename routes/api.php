b2985eb03b97d641015019bb6b01a9322737709d
Michael Eisenbraun <krepta00@gmail.com>
2022-03-27 19:22:12 +0000

    Fixes CORS issues

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 function getHex ($num) {
     $hash = hash("md5", $num, true);
